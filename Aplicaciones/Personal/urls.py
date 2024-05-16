e09aa3e8c1e3cbe6c00296ae6323ca7998b5577c
d4rw1nr <darwinjo6.4@gmail.com>
2024-05-16 00:23:30 -0500

    API configuration

--- Aplicaciones/Personal/urls.py
+++ Aplicaciones/Personal/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import StaffViewSet
 
 router = DefaultRouter()
-router.register(r'staff', DefaultRouter)
+router.register(r'staff', StaffViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
